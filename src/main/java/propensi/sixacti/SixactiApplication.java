22af0522f8467e842f7bd2054c64dbf62edfa8ed
Fivi Melinda <fivimelinda@gmail.com>
2020-03-08 13:38:11 +0700

    initialize spring boot project

---
+++ src/main/java/propensi/sixacti/SixactiApplication.java
@@ -0,0 +1,13 @@
+package propensi.sixacti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixactiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixactiApplication.class, args);
+	}
+
+}
