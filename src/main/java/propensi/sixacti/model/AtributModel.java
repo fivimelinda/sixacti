88f0ed71a02ea8ea23f7abaa031bcd3fdbbbb627
Nur Rifandy <nur.rifandy@ui.ac.id>
2020-03-11 21:14:06 +0700

    rancangan model

---
+++ src/main/java/propensi/sixacti/model/AtributModel.java
@@ -0,0 +1,13 @@
+package propensi.sixacti.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "atribut")
+
+public class AtributModel implements Serializable{
+
+}
---
+++ src/main/java/propensi/sixacti/model/TesMedisModel.java
@@ -0,0 +1,14 @@
+package propensi.sixacti.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tesMedis")
+
+public class TesMedisModel implements Serializable{
+
+    
+}
