bc42475955043872d599efee1b4aed6248894916
Nur Rifandy <nur.rifandy@ui.ac.id>
2020-03-11 22:29:15 +0700

    konfigurasi pada  application properties

--- src/main/java/propensi/sixacti/model/TesMedisModel.java
+++ src/main/java/propensi/sixacti/model/TesMedisModel.java
@@ -3,6 +3,9 @@ package propensi.sixacti.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -10,5 +13,7 @@ import javax.persistence.Table;
 
 public class TesMedisModel implements Serializable{
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTesMedis;
 }
