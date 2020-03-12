5adfd1c1be4a5bc9e9d514dcb0565cd53610f525
Nur Rifandy <nur.rifandy@ui.ac.id>
2020-03-12 19:21:02 +0700

    membuat model tes

--- src/main/java/propensi/sixacti/model/TesMedisModel.java
+++ src/main/java/propensi/sixacti/model/TesMedisModel.java
@@ -2,11 +2,14 @@ package propensi.sixacti.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tesMedis")
@@ -16,4 +19,114 @@ public class TesMedisModel implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idTesMedis;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "tinggiBadan", nullable = false)
+    private String tinggiBadan;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "beratBadan", nullable = false)
+    private String beratBadan;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "tekananDarah", nullable = false)
+    private String tekananDarah;
+  
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "butaWarna", nullable = false)
+    private Boolean butaWarna;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "riwayatPenyakit", nullable = false)
+    private String riwayatPenyakit;
+    
+    /**
+     * @return the idTesMedis
+     */
+    public Long getIdTesMedis() {
+        return idTesMedis;
+    }
+
+    /**
+     * @param idTesMedis the idTesMedis to set
+     */
+    public void setIdTesMedis(Long idTesMedis) {
+        this.idTesMedis = idTesMedis;
+    }
+
+    /**
+     * @return the tinggiBadan
+     */
+    public String getTinggiBadan() {
+        return tinggiBadan;
+    }
+
+    /**
+     * @param tinggiBadan the tinggiBadan to set
+     */
+    public void setTinggiBadan(String tinggiBadan) {
+        this.tinggiBadan = tinggiBadan;
+    }
+
+    /**
+     * @return the beratBadan
+     */
+    public String getBeratBadan() {
+        return beratBadan;
+    }
+
+    /**
+     * @param beratBadan the beratBadan to set
+     */
+    public void setBeratBadan(String beratBadan) {
+        this.beratBadan = beratBadan;
+    }
+
+    /**
+     * @return the butaWarna
+     */
+    public Boolean getButaWarna() {
+        return butaWarna;
+    }
+    
+    /**
+     * @param butaWarna the butaWarna to set
+     */
+    public void setButaWarna(Boolean butaWarna) {
+        this.butaWarna = butaWarna;
+    }
+
+    /**
+     * @return the tekananDarah
+     */
+    public String getTekananDarah() {
+        return tekananDarah;
+    }
+
+    /**
+     * @param tekananDarah the tekananDarah to set
+     */
+    public void setTekananDarah(String tekananDarah) {
+        this.tekananDarah = tekananDarah;
+    }
+
+    /**
+     * @return the riwayatPenyakit
+     */
+    public String getRiwayatPenyakit() {
+        return riwayatPenyakit;
+    }
+
+    /**
+     * @param riwayatPenyakit the riwayatPenyakit to set
+     */
+    public void setRiwayatPenyakit(String riwayatPenyakit) {
+        this.riwayatPenyakit = riwayatPenyakit;
+    }
+
 }
--- src/main/java/propensi/sixacti/model/TesWawancaraModel.java
+++ src/main/java/propensi/sixacti/model/TesWawancaraModel.java
@@ -2,11 +2,14 @@ package propensi.sixacti.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tesWawancara")
@@ -16,4 +19,57 @@ public class TesWawancaraModel implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idTesWawancara;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "nilai", nullable = false)
+    private String nilai;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "feedback", nullable = false)
+    private String feedback;
+
+    /**
+     * @return the idTesWawancara
+     */
+    public Long getIdTesWawancara() {
+        return idTesWawancara;
+    }
+
+    /**
+     * @param idTesWawancara the idTesWawancara to set
+     */
+    public void setIdTesWawancara(Long idTesWawancara) {
+        this.idTesWawancara = idTesWawancara;
+    }
+
+    /**
+     * @return the nilai
+     */
+    public String getNilai() {
+        return nilai;
+    }
+
+    /**
+     * @param nilai the nilai to set
+     */
+    public void setNilai(String nilai) {
+        this.nilai = nilai;
+    }
+
+    /**
+     * @return the feedback
+     */
+    public String getFeedback() {
+        return feedback;
+    }
+
+    /**
+     * @param feedback the feedback to set
+     */
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+    
 }
