1a92e52e270c65f7918afdf4b494cf9473d533c6
fivimelinda <54924175+fivimelinda@users.noreply.github.com>
2020-05-08 14:11:29 +0700

    Delete KaryawanDb.java

--- src/main/java/propensi/sixacti/repository/KaryawanDb.java
+++
@@ -1,9 +0,0 @@
-package propensi.sixacti.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import propensi.sixacti.model.KaryawanModel;
-
-public interface KaryawanDB extends JpaRepository<KaryawanModel, Long> {
-    
-}
