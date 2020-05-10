5b87cb9b8061452aef232ebe5bff31a6d3370f8e
Nur Rifandy <nur.rifandy@ui.ac.id>
2020-05-10 09:02:50 +0700

    atributDb minor

--- src/main/java/propensi/sixacti/repository/AtributDb.java
+++ src/main/java/propensi/sixacti/repository/AtributDb.java
@@ -10,5 +10,7 @@ import propensi.sixacti.model.KaryawanModel;
 
 @Repository
 public interface AtributDb extends JpaRepository<AtributModel, Long>{
+
     List<AtributModel> findByKaryawan(KaryawanModel karyawan);
+    
 }
