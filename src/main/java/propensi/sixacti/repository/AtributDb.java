eaa9626ed919d173d94798957f10727390eace0c
Grahana Daffa Herlambang <grahanadaffa1@gmail.com>
2020-05-16 13:09:51 +0700

    conflict res

--- src/main/java/propensi/sixacti/repository/AtributDb.java
+++ src/main/java/propensi/sixacti/repository/AtributDb.java
@@ -1,8 +1,11 @@
 package propensi.sixacti.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import propensi.sixacti.model.AtributModel;
+import propensi.sixacti.model.KaryawanModel;
 
 @Repository
 public interface AtributDb extends JpaRepository<AtributModel, Long>{
